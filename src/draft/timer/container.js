8826f43a53ee42eaebb6aa633611f89d04a98eaf
Nguyen Thang <ngenvan000@gmail.com>
2022-05-29 22:48:00 +0700

    update

--- src/draft/timer/container.js
+++ src/draft/timer/container.js
@@ -1,3 +1,42 @@
-function C(argument) {
-	// body...
+import { useReducer } from "react";
+
+//init  state
+let initState = 0;
+
+// Actions
+const UP_ACTION = "up_action";
+const DOWN_ACTION = "down_action";
+
+//Reducer
+const reducer = (state, action) => {
+	switch (action) {
+		case UP_ACTION:
+			return state + 1;
+		case DOWN_ACTION:
+			return state - 1;
+		default:
+			throw new Error("invalid action");
+	}
+};
+
+//dispatch
+
+function Container() {
+	const [count, dispatch] = useReducer(reducer, initState);
+	return (
+		<div style={{ margin: "20px" }}>
+			<h1
+				style={{
+					padding: "20px",
+					paddingBottom: "0",
+					fontSize: "60px",
+				}}
+			>
+				{count}
+			</h1>
+			<button onClick={() => dispatch(DOWN_ACTION)}>down</button>
+			<button onClick={() => dispatch(UP_ACTION)}>up</button>
+		</div>
+	);
 }
+export default Container;
