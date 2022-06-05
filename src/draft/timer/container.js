86d06ff56cdfb401648a35306a09c5f8e3cbfcb4
Nguyen Thang <ngenvan000@gmail.com>
2022-06-05 22:54:34 +0700

    update

--- src/draft/timer/container.js
+++ src/draft/timer/container.js
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+import { useReducer, useRef } from "react";
 
 //init  state
 const initState = {
@@ -8,15 +8,22 @@ const initState = {
 // Actions
 const SETTASK = "set_task";
 const ADDTASK = "add_task";
-const settask = (payload) => {
+const DELETETASK = "delete_task";
+const setTask = (payload) => {
 	return {
-		type: "set_task",
+		type: SETTASK,
 		payload,
 	};
 };
-const addtask = () => {
+const addTask = () => {
 	return {
-		type: "add_task",
+		type: ADDTASK,
+	};
+};
+const deleteTask = (pos) => {
+	return {
+		type: DELETETASK,
+		pos,
 	};
 };
 //Reducer
@@ -27,13 +34,15 @@ const reducer = (state, action) => {
 				...state,
 				job: action.payload,
 			};
-			break;
 		case ADDTASK:
 			if (state.job === "") return state;
 			return {
 				jobs: [...state.jobs, state.job],
 				job: "",
 			};
+		case DELETETASK:
+			state.jobs.splice(action.pos, 1);
+			return { ...state };
 		default:
 			throw new Error("invalid action");
 	}
@@ -44,19 +53,40 @@ const reducer = (state, action) => {
 function Container() {
 	const [state, dispatch] = useReducer(reducer, initState);
 	const { job, jobs } = state;
+	const inputRef = useRef();
 	return (
 		<div style={{ margin: "20px" }}>
 			<h1>ToDo</h1>
 			<input
 				type="text"
 				value={job}
+				ref={inputRef}
 				placeholder="please todo"
-				onChange={(e) => dispatch(settask(e.target.value))}
+				onChange={(e) => dispatch(setTask(e.target.value))}
 			/>
-			<button onClick={() => dispatch(addtask())}>add</button>
+			<button
+				onClick={() => {
+					dispatch(addTask());
+					inputRef.current.focus();
+				}}
+			>
+				add
+			</button>
 			<ul>
-				{jobs.map((job) => (
-					<li key={job}>{job}</li>
+				{jobs.map((job, i) => (
+					<div
+						key={i}
+						style={{
+							display: "flex",
+							justifyContent: "space-between",
+							width: "150px",
+						}}
+					>
+						<li>{job}</li>
+						<button onClick={() => dispatch(deleteTask(i))}>
+							x
+						</button>
+					</div>
 				))}
 			</ul>
 		</div>
