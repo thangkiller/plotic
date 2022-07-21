e56328c0e79117764f5f930951f3a72c79dfe432
Nguyen Thang <ngenvan000@gmail.com>
2022-07-21 19:57:16 +0700

    learning react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import { Routes, Route, Link } from "react-router-dom";
+import HomePage from "./pages/Home";
+import BlogPage from "./pages/Blog";
+import ContactPage from "./pages/Contact";
 import "./App.css";
-import Container from "./draft/timer/Container";
 
 function App() {
   return (
-    <div>
-      <Container />
-    </div>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/Blog">Blog</Link>
+        </li>
+        <li>
+          <Link to="/Contact">Contact</Link>
+        </li>
+      </ul>
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/Blog" element={<BlogPage />} />
+        <Route path="/Contact" element={<ContactPage />} />
+      </Routes>
+    </nav>
   );
 }
 export default App;
